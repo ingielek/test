52f26d8767a427d1798264d53c06058884f7aec2
ingielek <ingielewicz.p@gmail.com>
2018-07-10 20:50:58 +0200

    Starting project, first push

---
+++ src/components/navbar.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import '../App.css';
+import { Navbar, NavItem, MenuItem, Nav, NavDropdown } from 'react-bootstrap';
+
+
+export default class NavBar extends Component {
+    render() {
+        return (
+            <Navbar inverse collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#brand">Find My Pet!</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavItem eventKey={1} href="#">
+                            Post Lost Pet
+                        </NavItem>
+                        <NavItem eventKey={2} href="#">
+                            Look For Lost Pet
+                        </NavItem>
+                        <NavDropdown eventKey={3} title="Menu" id="basic-nav-dropdown">
+                            <MenuItem eventKey={3.1}>Action</MenuItem>
+                            <MenuItem eventKey={3.2}>Another action</MenuItem>
+                            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                            <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav pullRight>
+                        <NavItem eventKey={1} href="#">
+                            Nearest Pet Shelter
+                        </NavItem>
+                        <NavItem eventKey={2} href="#">
+                            Notify Nearest Pet Shelter
+                        </NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
+}
